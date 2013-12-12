d57cdc7dd3b240bcb1a86db4ebfbb2814cdabc60
drakoz <mat.mello93@gmail.com>
2013-12-12 21:54:47 -0200

    GUI

--- src/estacionamento/Comportamentos.java
+++ src/estacionamento/Comportamentos.java
@@ -2,6 +2,8 @@ package estacionamento;
 
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 
 public class Comportamentos extends Behaviour {
 	
---
+++ src/estacionamento/Gui.java
@@ -0,0 +1,13 @@
+package estacionamento;
+
+import java.awt.*;
+import javax.swing.*;
+import java.beans.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+import jade.core.behaviours.OneShotBehaviour;
+
+public class Gui extends JFrame {
+
+}
